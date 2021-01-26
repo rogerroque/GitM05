bc1a4d98d75edb8fe56191eb56be57f6f73336cc
rogerroque <reroqueo01@elpuig.xeill.net>
2021-01-26 16:15:49 +0100

    ggfg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hola mundo");
-<<<<<<< HEAD
         System.out.println("Second push");
-=======
-        System.out.println("First push");
->>>>>>> d49c81b246fc17d4d546c0bbb4a1aa19266668b2
 
     }
 }
