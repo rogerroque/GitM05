18b17ba832d3961bd09c9b4a9abd2f39c30db01f
rogerroque <reroqueo01@elpuig.xeill.net>
2021-01-26 15:53:28 +0100

    Primer push

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hola mundo");
-        System.out.println("Adios mundo");
+        System.out.println("Primer push");
 
     }
 }
