158f80392c589aaebe8fdc9730adb69ac332a068
rogerroque <reroqueo01@elpuig.xeill.net>
2021-01-30 03:50:14 +0100

    push branch2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,13 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hola mundo");
-<<<<<<< HEAD
 
         System.out.println("Second push");
 
-=======
         System.out.println("First push");
->>>>>>> 7c9df54ffc4713ceadd798de32b8c8ee674cba62
+
+        System.out.println("Branch 2");
+
 
     }
 }
